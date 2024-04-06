97fbe67c84edf3fdb14b1e4bc563b1feec0ca2e0
prabhjyot009 <prabh156dps@gmail.com>
2024-04-06 22:54:31 +0530

    Update hero image and logo in Hero.tsx component, and add AOS animation library in OurBurgers.tsx component

--- burger/components/Hero.tsx
+++ burger/components/Hero.tsx
@@ -18,16 +18,16 @@ const Hero = () => {
     <section id='hero' className='w-full flex lg:flex-row flex-col
     justify-between items-start'>
       <div id='image-section' className='lg:w-[100%] w-full'>
-        <Image data-aos="zoom-in" src="/MainBanner.png" alt='hero-banner' 
-        width={1100} height={500} className='bg-cover bg-center'/>
+        <Image data-aos="zoom-in" src="/Hero.jpg" alt='hero-banner' 
+        width={1200} height={500} className='bg-cover bg-center'/>
       </div>
       <div id='content-section' className='lg:w-[50%] w-full flex
-      flex-col justify-center items-center gap-10 pl-10 pr-10 pt-10'>
-      <Image src='/Logo.png' alt='logo' width={120} height={100} data-aos="zoom-in"
+      flex-col justify-center items-center gap-10 p-20'>
+      <Image src='/logo.png' alt='logo' width={120} height={100} data-aos="zoom-in"
       data-aos-delay="200" className='rounded-full size-100'/>
       <h1 data-aos='zoom-in' data-aos-delay='400' className='text-[50px] text-black text-center leading-[55px]'>Fresh Buns<br></br> Handcrafted daily</h1>
       <p data-aos='zoom-in' data-aos-delay='600' className='text-xl text-slate-600 text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      <button data-aos='zoom-in' data-aos-delay='800' className=' border-2 text-black text-lg rounded-full px-14 py-4'>READ MORE</button>
+      <button data-aos='zoom-in' data-aos-delay='800' className=' border-2 border-black text-black text-lg rounded-full px-14 py-4 font-bold'>READ MORE</button>
       </div>
     </section>
   )
--- burger/components/OurBurgers.tsx
+++ burger/components/OurBurgers.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
+import AOS  from 'aos';
+import 'aos/dist/aos.css';
+import Image from 'next/image';
+import { CanvasRevealEffect } from '@/components/ui/canvas-reveal-effect'
+import { ourbreads } from '@/export';
+
 
 const OurBurgers = () => {
+  useEffect(()=>{
+    AOS.init({
+      duration:500,
+      offset:200,
+      easing:'ease-in-sine',
+    })
+  },[])
   return (
-    <div>OurBurgers</div>
+    <section id='breads' className='w-full flex flex-col justify-center items-center gap-10 bg-cover h-fit py-20 lg:px-20 px-10 bg-center' style={{ backgroundImage:`url('/banner4.jpg')`}}
+    >
+      <h1 data-aos="zoom-in" className='text-[55px] text-white font-semibold'>Our Breads</h1>
+      <div data-aos='zoom-in' data-aos-delay="200" className='w-full grid lg:grid-cols-4 grid-cols-1 justify-center items-center gap-6'>
+        {ourbreads.map((item,index)=>(
+          <div key={index} className='flex flex-col justify-center items-center gap-4 bg-white p-8 rounded-lg'>
+            <Image src={item.image} alt='bread image' width={300} height={400} className='rounded-full w-[300px] h-[200px] rounded-lg transform hover:scale-110 transition-transform duration-300 cursor-pointer'/>
+            <h1 className='text-black text-lg font-semibold'>{item.title}</h1>
+            <p className='text-center'>{item.desc}</p>
+            <button className='border-2 border-black rounded-full px-8 py-2 uppercase text-sm font-semibold transform hover:scale-110 transition-transform duration-300'>ORDER NOW</button>
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
