7c81057165eff9740087a13498f1b8f7cb517bd6
prabhjyot009 <prabh156dps@gmail.com>
2024-04-08 20:27:30 +0530

    Update WhyUs component in burger package

--- burger/components/WhyUs.tsx
+++ burger/components/WhyUs.tsx
@@ -17,10 +17,10 @@ const WhyUs = () => {
             <h1 data-aos="zoom-in" className='text-[55px] text-black text-center leading-[55px]'>Where Every Treat<br/>Tells a Tale</h1>
             <p data-aos="zoom-in" data-aos-display="200" className='text-center text-xl text-slate-600'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident blanditiis similique repellat ducimus, distinctio dolore architecto harum reiciendis tempora in quas illum laudantium culpa. Beatae sunt consequatur omnis eveniet atque.</p>
             <button data-aos="flip-up" data-aos-display="400" className='border-2 border-black rounded-full px-14 py-4 font-bold'>READ MORE</button>
-            <div data-aos="zoom-in" data-aos-display="600" id='image-section' className='lg:w-[50%] w-full'>
+          </div>
+          <div data-aos="zoom-in" data-aos-display="600" id='image-section' className='lg:w-[50%] w-full'>
               <Image src="/banner6.jpg" alt='hero-banner' width={800} height={500} className='bg-cover bg-center'/>
             </div>
-          </div>
     </section>
   )
 }
